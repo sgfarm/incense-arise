4a17f38bf6b538167a9a92233126255840087540
John Lewis <github@jlewis.sh>
2024-04-24 17:20:14 +0200

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils}: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs = (import nixpkgs) {
+          inherit system overlays;
+        };
+
+        playlist = ./playlist.m3u;
+
+        container = pkgs.dockerTools.buildLayeredImage {
+          name = "incense-arise";
+          tag = "latest";
+          contents = [ pkgs.cacert ];
+          config = {
+            # runs the executable with tini: https://github.com/krallin/tini
+            # this does signal forwarding and zombie process reaping
+            Entrypoint = [ "${pkgs.tini}/bin/tini" "--" ];
+            User = "1000";
+            Cmd = [
+              "${pkgs.vlc}/bin/cvlc" "${playlist}" "-v" "--loop" "--random"
+              "--sout-all" "--sout-keep" "--sout"
+              "#transcode{vcodec=h264,vb=800,acodec=mpga,ab=128,channels=2,samplerate=44100
+,scodec=none}:http{mux=ts,dst=:3000/}"
+            ];
+          };
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            just yt-dlp awscli vlc flyctl
+          ];
+        };
+        packages = {
+          inherit container;
+          default = container;
+        };
+      });
+}
---
+++ justfile
@@ -0,0 +1,25 @@
+YT_PLAYLIST_URL := "https://www.youtube.com/watch?v=rYQ5yXCc_CA&list=PL4kZATn_awL77rh3xUQBWKEj1gNlkv1Zw"
+S3_BUCKET_NAME := "sgfarm-incense-arise"
+URL_PREFIX := "http://" + S3_BUCKET_NAME + ".s3.us-east-2.amazonaws.com/"
+
+_list:
+    @just --list
+
+# downloads YT videos, syncs them to S3, and generates the list
+update: download sync write-list
+
+# downloads YT videos from playlist `YT_PLAYLIST_URL` into `./video_cache/`
+download:
+    -yt-dlp --no-progress --no-simulate -P "./video_cache" -o "%(title)s.%(ext)s" --restrict-filenames --embed-subs --sub-langs "en.*" "{{ YT_PLAYLIST_URL }}"
+
+# syncs S3 bucket with `./video_cache/`
+sync:
+    aws s3 sync video_cache s3://{{ S3_BUCKET_NAME }}/ --delete
+
+# writes an m3u playlist with S3 URLS from `./video_cache/`
+write-list:
+    ls -1 ./video_cache | sed "s|^|{{ URL_PREFIX }}|g" > playlist.m3u
+
+# builds and runs the server container
+container:
+	nix build "./#container" && docker load -i result && docker run --rm -p 3000:3000 incense-arise
